ebea7383e7b087de182e32b3f0ba25b636a0fc9d
neto_alcides <alcides.carlos@yahoo.com.br>
2018-11-11 20:14:16 -0500

    testes VaR

--- src/Tests/VaR_Analysis.R
+++ src/Tests/VaR_Analysis.R
@@ -1,16 +1,52 @@
 info( logger, "HAR_NEURAL_PROJECT::VaR analysis" )
 
 info( logger, "HAR_NEURAL_PROJECT::VaR analysis fixed window models" )
-load(file = "cache/fixed_window.results_forecasts_har_classic_by_horizon.RData")
 
-fixed_window.var_forecasts_har_classic_by_horizon <- fixed_window.results_forecasts_har_classic_by_horizon %>%
-  mutate( date = as_date(date) ) %>% 
-  mutate_at( vars( contains('_h') ),
-             funs( parametric_var(volatility = sqrt(./252), alpha = 0.05) ) ) %>% # parametric var
-  rename_at( vars(contains('_h') ),
-             funs( paste0('var_', .) ) ) %>% 
-  left_join(., bvsp_rv5 %>% select( date, close_price, log_return ),
-            by = 'date' ) %>% 
-  select( -rv5_252, -close_price )
+# carrega dados
+list.files( "./cache", pattern = 'fixed_window.RData', full.names = TRUE ) %>% 
+  lapply(., load, .GlobalEnv )
 
-rm(fixed_window.results_forecasts_har_classic_by_horizon)
+# estima o var
+models_var <- bind_rows( results_forecasts_har_classic_by_horizon.fixed_window %>% 
+                                   mutate( model = paste0('har_classic') ),
+                                 results_forecasts_har_stepwise_aic_by_horizon.fixed_window %>% 
+                                   mutate( model = paste0('har_stepwise_aic') ),
+                                 results_forecasts_har_stepwise_bic_by_horizon.fixed_window %>% 
+                                   mutate( model = paste0('har_stepwise_bic') )
+)
+
+alpha_ = 0.01
+
+models_var %<>% 
+  mutate( prediction = ifelse( prediction < 0, 0, prediction ),
+          VaR = -parametric_var(volatility = sqrt(prediction/252), alpha = alpha_ ) )
+
+horizons_test <- models_var %>% 
+  distinct( pred_horizon )
+
+models_test <- models_var %>% 
+  distinct( model )
+
+# realiza os testes
+var_test_results <- foreach( horizons = horizons_test$pred_horizon, .combine = rbind ) %:% 
+  foreach( models = models_test$model, .combine = rbind ) %dopar% {
+  
+    var <- models_var %>% 
+    filter( pred_horizon == horizons,
+            model == models )
+    
+    var_test <- VaRTest( alpha = alpha_, 
+             actual = har_original_data_structure_valid$log_return, 
+             VaR = var$VaR,
+             conf.level =  0.95 )
+    
+    data_frame( model = models,
+                pred_horizon = horizons,
+                viol_ratio = 100 * mean( ifelse( har_original_data_structure_valid$log_return < var$VaR, 1, 0 ) ),
+                asmf = 100 * (sum( ifelse( har_original_data_structure_valid$log_return < var$VaR, (har_original_data_structure_valid$log_return - var$VaR)^2, 0 ) ) / sum( ifelse( har_original_data_structure_valid$log_return < var$VaR, 1, 0 ) )),
+                lr_uc = paste0( round(var_test$uc.LRstat, 3), "(", round(var_test$uc.LRp, 3), ")" ),
+                lr_cc = paste0( round(var_test$cc.LRstat, 3), "(", round(var_test$cc.LRp, 3), ")" ))
+}
+
+
+
